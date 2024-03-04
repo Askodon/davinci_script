9d2f65c43dda99011d081f055b75b1db2e4c9b87
Askodon <askodonte@proton.me>
2024-03-04 11:35:46 +0400

    reanme

---
+++ mov_script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#this is davinci resolve convert script
+#please check if zenity and ffmpeg install on your distro
+
+
+#open file.
+FILE=`zenity --file-selection --title="select file to convert"`
+
+#save file. enter the name and ".mov".
+zenity --info \
+       --title "Info Message" \
+       --width 300 \
+       --height 100 \
+       --text "choose end path and make file with ".mov""
+DIR=`zenity --file-selection --save --filename="/home/${USER}/"`
+
+#convert file to work on davinci.
+ffmpeg -i $FILE -c:v dnxhd -profile:v dnxhr_hq -pix_fmt yuv422p -c:a pcm_s16le -f mov $DIR
+
+
+
