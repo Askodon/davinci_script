9a321a1d78d66780e6a89410262e7f2547bcc3f3
Askodon <askodonte@proton.me>
2024-03-04 15:56:32 +0400

    fix comments

--- alias.sh
+++ alias.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-#write this alias in .bashrc
+#auto paste alias to use withiou cd on dir
+#type "davinci" on terminal
 WORK_DIR=$(pwd)
 echo -e "\nalias davinci='$WORK_DIR/"mov_script.sh"'" >> ~/.bashrc
