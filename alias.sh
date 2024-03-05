24a61173da60a1af2ba56c83c513190fa6289683
Askodon <askodonte@proton.me>
2024-03-05 16:28:07 +0400

    minor fixes

--- alias.sh
+++ alias.sh
@@ -2,4 +2,6 @@
 #auto paste alias to use without cd to dir.
 #type "davinci" on terminal.
 WORK_DIR=$(pwd)
+
+#change if use zsh to ~/.zshrc
 echo -e "\nalias davinci='$WORK_DIR/"mov_script.sh"'" >> ~/.bashrc
